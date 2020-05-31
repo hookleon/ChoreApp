b815b30f9b91b2ee574d646e0bf269efe8882c30
hookleon <hookleon@gmaill.com>
2020-05-31 19:30:09 +1200

    app now just needs to connect the info that is inputted into the final chorelist activity
    this will allow users to view their names alongside chores for the week

--- app/src/main/java/com/example/choreapp/AddHouseMemberActivity.java
+++ app/src/main/java/com/example/choreapp/AddHouseMemberActivity.java
@@ -76,9 +76,9 @@ public class AddHouseMemberActivity extends AppCompatActivity{
         //Adds all members under the name of new household
         EditText editHouse = (EditText) findViewById(R.id.editHouse);
         String house = editHouse.getText().toString();
-        for (int i = 0; i < members.size(); i++){
-            mRef.child("groups").child(houseID).child("members").setValue(members.get(i).getID());
-        }
+        //for (int i = 0; i < members.size(); i++){
+            mRef.child("groups").child(houseID).child("members").setValue(members);
+        //}
         mRef.child("groups").child(houseID).child("name").setValue(house);
         // Moves to the next page where you pick chores
         Intent intent = new Intent(this, AddChoresActivity.class);
