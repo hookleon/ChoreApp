53f391d874d7be483d6a0cd7bfd8ea2c18b612a8
hookleon <63945425+hookleon@users.noreply.github.com>
2020-07-17 10:04:24 +1200

    Update AddChoresActivity.java

--- app/src/main/java/com/example/choreapp/AddChoresActivity.java
+++ app/src/main/java/com/example/choreapp/AddChoresActivity.java
@@ -10,9 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
