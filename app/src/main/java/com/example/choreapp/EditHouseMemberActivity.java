320deea705df37b4bd779e556d86543d7495fd3e
hookleon <hookleon@gmaill.com>
2020-08-07 13:30:01 +1200

    Removed unused imports Fixed Codacy issues

--- app/src/main/java/com/example/choreapp/EditHouseMemberActivity.java
+++ app/src/main/java/com/example/choreapp/EditHouseMemberActivity.java
@@ -12,7 +12,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
